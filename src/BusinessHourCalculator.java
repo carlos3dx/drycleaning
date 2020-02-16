079a17c45d1ece1027cf8219de942124a27e77dd
Carlos Villar <carlosbetera3dx@gmail.com>
2020-02-16 10:50:42 +0100

    First commit, skeleton created

---
+++ src/BusinessHourCalculator.java
@@ -0,0 +1,29 @@
+import java.time.DayOfWeek;
+import java.util.Date;
+
+public class BusinessHourCalculator {
+    BusinessHourCalculator(final String defaultOpeningTime, final String defaultClosingTime) {
+
+    }
+
+    public void setOpeningHours(final DayOfWeek dayOfWeek, final String openingTime, final String closingTime) {
+
+    }
+
+    public void setOpeningHours(final String date, final String openingTime, final String closingTime) {
+
+    }
+
+    public void setClosed(final DayOfWeek... daysOfWeek) {
+
+    }
+
+    public void setClosed(final String... dates) {
+
+    }
+
+    public Date calculateDeadline(final long duration, final String startTime){
+        return null;
+    }
+
+}
