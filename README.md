c550db527fad3a88a9d8666d88488c379753efda
Carlos Villar <carlosbetera3dx@gmail.com>
2020-02-18 19:31:50 +0100

    Readme and cleanup

---
+++ README.md
@@ -0,0 +1,4 @@
+#Instructions
+
+The class `BusinessHourCalculator` can be found at `src` folder, in `testsrc` there is a test to check the class against the given examples.
+
--- src/WeekDay.java
+++ src/WeekDay.java
@@ -6,8 +6,6 @@ public class WeekDay {
     private LocalTime openingHour;
     private LocalTime closingHour;
 
-    private static final String TIME_FORMAT = "kk:mm";
-
     public WeekDay() {
         this(false, "00:00", "23:59");
     }
@@ -36,19 +34,11 @@ public class WeekDay {
         return LocalTime.parse(time.length() > 4 ? time : "0" + time);
     }
 
-    public long timeOpen(){
-        return calculateTimeUntilClose(getOpeningHour());
-    }
-
-    public long calculateTimeUntilClose(final String time) {
-        return calculateTimeUntilClose(parseLocalTime(time));
-    }
-
     public long calculateTimeUntilClose(final LocalTime time) {
         final long result;
         if (isOpen() && time.isBefore(closingHour)) {
 
-            final Duration duration = Duration.between(time.isAfter(openingHour)?time:openingHour, closingHour);
+            final Duration duration = Duration.between(time.isAfter(openingHour) ? time : openingHour, closingHour);
             result = duration.getSeconds();
         } else {
             result = 0;
